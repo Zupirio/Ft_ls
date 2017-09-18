467da6aaeb36b66c6eca9f450dea52497b1c1c65
Asahel RANGARIRA <arangari@e4r13p4.wethinkcode.co.za>
2017-09-18 18:47:32 +0200

    Fixed a few typos plusu missing '?' conditional statement

--- ft_do_file_check.c
+++ ft_do_file_check.c
@@ -6,19 +6,19 @@
 /*   By: arangari <arangari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/14 06:44:33 by arangari          #+#    #+#             */
-/*   Updated: 2017/09/14 06:59:39 by arangari         ###   ########.fr       */
+/*   Updated: 2017/09/18 18:45:32 by arangari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	do_ls(char *dir, t_env *e)
+void		do_ls(char *dir, t_env *e)
 {
 	t_dir	*list;
 	t_dir	*tp;
 
 	list = NULL;
-	if ((e->dp = opendir(dir)) != NULL)
+	if ((e->dirp = opendir(dir)) != NULL)
 	{
 		while ((e->dp = readdir(e->dirp)) != NULL)
 		{
@@ -28,20 +28,20 @@ void	do_ls(char *dir, t_env *e)
 				tp->dir = e->dp->d_name;
 				tp->full_path = ft_strjoin(ft_strjoin(dir, "/"), e->dp->d_name);
 				lstat(tp->full_path, &e->sb);
-				tp->time = e->sb.st.st_mtime;
-				(S_ISDIR(e->sb.st_mode) == 1)  tp->is_dir = 1 : 0;
+				tp->time = e->sb.st_mtime;
+				(S_ISDIR(e->sb.st_mode) == 1) ? tp->is_dir = 1 : 0;
 				tp->next = list;
 				list = tp;
 			}
 		}
 		sort_list(&list, compare_strings, 0);
-		ft_print_ls(link, e, dir);
+		ft_print_ls(list, e, dir);
 		return ;
 	}
 	ft_file_check(e);
 }
 
-void	ft_ls(t_env *e)
+void		ft_ls(t_env *e)
 {
 	t_dir	*tp;
 
